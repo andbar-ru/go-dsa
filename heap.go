c11043629b07b1aee51b53c94b0c081dbf268f2f
Andrei Bartashevich <andrei.bartashevich@sitronics-kt.ru>
2023-11-19 03:31:40 +0300

    Fibonacci Heap

--- heap.go
+++ heap.go
@@ -1,5 +1,14 @@
 package dsa
 
+import (
+	"fmt"
+	"strconv"
+)
+
+/**************************************************
+ * Heap
+ **************************************************/
+
 type Heap struct {
 	arr []int
 }
@@ -52,4 +61,205 @@ func (h *Heap) Delete(num int) {
 	for i = size/2 - 1; i >= 0; i-- {
 		h.Heapify(i)
 	}
+}
+
+/**************************************************
+ * Fibonacci Heap
+ **************************************************/
+
+type FibonacciHeapNode struct {
+	parent *FibonacciHeapNode
+	left   *FibonacciHeapNode
+	right  *FibonacciHeapNode
+	child  *FibonacciHeapNode
+	degree int
+	mark   bool
+	key    int
+}
+
+func NewFibonacciHeapNode(key int) *FibonacciHeapNode {
+	node := &FibonacciHeapNode{
+		degree: 0,
+		mark:   false,
+		parent: nil,
+		child:  nil,
+		key:    key,
+	}
+	node.left = node
+	node.right = node
+	return node
+}
+
+func (n *FibonacciHeapNode) String() string {
+	s := "("
+	if n == nil {
+		s += ")"
+		return s
+	}
+	curNode := n
+	first := true
+	for curNode != n || first {
+		if first {
+			s += "⟷"
+			first = false
+		}
+		s += " " + strconv.Itoa(curNode.key)
+		s += fmt.Sprint(curNode.child)
+		s += " ⟷"
+		curNode = curNode.right
+	}
+	s += ")"
+	return s
+}
+
+type FibonacciHeap struct {
+	min   *FibonacciHeapNode
+	trace bool
+	found *FibonacciHeapNode
+}
+
+func NewFibonacciHeap() *FibonacciHeap {
+	return &FibonacciHeap{}
+}
+
+func (h *FibonacciHeap) String() string {
+	if h == nil || h.min == nil {
+		return "()"
+	}
+	return fmt.Sprint(h.min)
+}
+
+func (h *FibonacciHeap) Insert(node *FibonacciHeapNode) {
+	if h == nil {
+		return
+	}
+	if h.min == nil {
+		h.min = node
+		node.left = node
+		node.right = node
+	} else {
+		node.right = h.min
+		node.left = h.min.left
+		h.min.left.right = node
+		h.min.left = node
+		if node.key < h.min.key {
+			h.min = node
+		}
+	}
+}
+
+func (h *FibonacciHeap) FindMin() (int, bool) {
+	if h == nil || h.min == nil {
+		return 0, false
+	}
+	return h.min.key, true
+}
+
+func (h *FibonacciHeap) ExtractMin() (int, bool) {
+	if h == nil || h.min == nil {
+		return 0, false
+	}
+	min := h.min
+	firstChild := min.child
+	curChild := firstChild
+	if firstChild != nil {
+		for ok := true; ok; ok = curChild != nil && curChild != firstChild { // similar to do..while
+			right := curChild.right
+			h.Insert(curChild)
+			curChild.parent = nil
+			curChild = right
+		}
+	}
+	min.left.right = min.right
+	min.right.left = min.left
+	min.child = nil
+	if min == min.right {
+		h.min = nil
+	} else {
+		h.min = min.right
+		h.consolidate()
+	}
+	return min.key, true
+}
+
+func (h *FibonacciHeap) consolidate() {
+	if h == nil || h.min == nil {
+		return
+	}
+	maxDegree := 0
+	d2n := make(map[int]*FibonacciHeapNode)
+	curNode := h.min
+	checkNode := h.min
+	for ok := true; ok; ok = curNode != nil && curNode != checkNode {
+		x := curNode
+		d := x.degree
+		for d2n[d] != nil {
+			y := d2n[d]
+			if x.key > y.key {
+				x, y = y, x
+				curNode = x
+			}
+			fibHeapLink(y, x)
+			delete(d2n, d)
+			if len(d2n) == 0 {
+				checkNode = x
+			}
+			d++
+		}
+		if d > maxDegree {
+			maxDegree = d
+		}
+		d2n[d] = x
+		curNode = curNode.right
+	}
+	h.min = nil
+	for i := 0; i <= maxDegree; i++ {
+		node := d2n[i]
+		if node != nil {
+			h.Insert(node)
+		}
+	}
+}
+
+func fibHeapLink(y, x *FibonacciHeapNode) {
+	y.left.right = y.right
+	y.right.left = y.left
+
+	c := x.child
+	if c == nil {
+		y.right = y
+		y.left = y
+	} else {
+		y.right = c
+		y.left = c.left
+		c.left.right = y
+		c.left = y
+	}
+	y.parent = x
+	x.child = y
+	x.degree = x.degree + 1
+	y.mark = false
+}
+
+func MergeHeaps(h1, h2 *FibonacciHeap) *FibonacciHeap {
+	if h1 == nil || h2 == nil {
+		if h1 == nil {
+			return h2
+		}
+		return h1
+	}
+	merged := NewFibonacciHeap()
+	merged.min = h1.min
+	if h1.min != nil && h2.min != nil {
+		t1 := h1.min.left
+		t2 := h2.min.left
+		h1.min.left = t2
+		t1.right = h2.min
+		h2.min.left = t1
+		t2.right = h1.min
+	}
+	if h1.min == nil || (h2.min != nil && h2.min.key < h1.min.key) {
+		merged.min = h2.min
+	}
+	return merged
 }
--- heap_test.go
+++ heap_test.go
@@ -1,9 +1,50 @@
 package dsa
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
+
+type fHeapNode struct {
+	key      int
+	children []*fHeapNode
+}
+
+func (n *fHeapNode) FibonacciHeapNode() *FibonacciHeapNode {
+	var processNode func(node, parent, left, right *FibonacciHeapNode, children []*fHeapNode)
+	processNode = func(node, parent, left, right *FibonacciHeapNode, children []*fHeapNode) {
+		if parent != nil {
+			node.parent = parent
+		}
+		if left == nil {
+			node.left = node
+		} else {
+			node.left = left
+			left.right = node
+		}
+		if right == nil {
+			node.right = node
+		} else {
+			node.right = right
+			right.left = node
+		}
+		var cLeft, cFirst *FibonacciHeapNode
+		for _, child := range children {
+			cNode := &FibonacciHeapNode{key: child.key, degree: len(child.children)}
+			processNode(cNode, node, cLeft, cFirst, child.children)
+			cLeft = cNode
+			if cFirst == nil {
+				cFirst = cNode
+				node.child = cFirst
+			}
+		}
+	}
+
+	node := &FibonacciHeapNode{key: n.key, degree: len(n.children)}
+	processNode(node, nil, nil, nil, n.children)
+	return node
+}
 
 func TestHeap(t *testing.T) {
 	heap := NewHeap()
@@ -22,5 +63,212 @@ func TestHeap(t *testing.T) {
 	want = []int{9, 5, 2, 3}
 	if !reflect.DeepEqual(heap.arr, want) {
 		t.Errorf("heap after deleting 4 = %v, want %v", heap.arr, want)
+	}
+}
+
+func walkFibonacciHeap(fheap *FibonacciHeap, handler func(*FibonacciHeapNode)) {
+	if fheap == nil {
+		return
+	}
+
+	var walkNode func(*FibonacciHeapNode)
+	walkNode = func(node *FibonacciHeapNode) {
+		if node == nil {
+			return
+		}
+		curNode := node
+		checkNode := node
+		for ok := true; ok; ok = curNode != checkNode {
+			handler(curNode)
+			if curNode.child != nil {
+				walkNode(curNode.child)
+			}
+			curNode = curNode.right
+		}
+	}
+
+	walkNode(fheap.min)
+}
+
+func TestFibonacciHeap(t *testing.T) {
+	fheap := NewFibonacciHeap()
+
+	res := fmt.Sprint(fheap)
+	want := "()"
+	if res != want {
+		t.Errorf("Empty fheap = %s, want %s", res, want)
+	}
+
+	fheap.Insert(NewFibonacciHeapNode(24))
+	res = fmt.Sprint(fheap)
+	want = "(⟷ 24() ⟷)"
+	if res != want {
+		t.Errorf("fheap = %s, want %s", res, want)
+	}
+	min := fheap.min.key
+	wantMin := 24
+	if min != wantMin {
+		t.Errorf("min = %d, want %d", min, wantMin)
+	}
+
+	fheap.Insert(NewFibonacciHeapNode(17))
+	fheap.Insert(NewFibonacciHeapNode(3))
+	fheap.Insert(NewFibonacciHeapNode(23))
+	fheap.Insert(NewFibonacciHeapNode(7))
+	res = fmt.Sprint(fheap)
+	want = "(⟷ 3() ⟷ 17() ⟷ 24() ⟷ 23() ⟷ 7() ⟷)"
+	if res != want {
+		t.Errorf("fheap = %s, want %s", res, want)
+	}
+	min = fheap.min.key
+	wantMin = 3
+	if min != wantMin {
+		t.Errorf("min = %d, want %d", min, wantMin)
+	}
+
+	// https://cppalgo.blogspot.com/2011/11/fibonacci-heap.html
+	fheap = NewFibonacciHeap()
+	fheap.Insert(NewFibonacciHeapNode(1))
+	fheap.Insert(NewFibonacciHeapNode(6))
+	fheap.Insert(NewFibonacciHeapNode(7))
+	fheap.Insert(NewFibonacciHeapNode(4))
+	fheap.Insert(NewFibonacciHeapNode(2))
+	fheap.Insert(NewFibonacciHeapNode(3))
+	fheap.Insert(NewFibonacciHeapNode(5))
+	res = fmt.Sprint(fheap)
+	want = "(⟷ 1() ⟷ 6() ⟷ 7() ⟷ 4() ⟷ 2() ⟷ 3() ⟷ 5() ⟷)"
+	if res != want {
+		t.Errorf("fheap = %s, want %s", res, want)
+	}
+	fheap.ExtractMin()
+	res = fmt.Sprint(fheap)
+	want = "(⟷ 2(⟷ 6(⟷ 7() ⟷) ⟷ 4() ⟷) ⟷ 3(⟷ 5() ⟷) ⟷)"
+	if res != want {
+		t.Errorf("fheap after ExtractMin = %s, want %s", res, want)
+	}
+	min = fheap.min.key
+	wantMin = 2
+	if min != wantMin {
+		t.Errorf("min = %d, want %d", min, wantMin)
+	}
+
+	k2d := map[int]int{3: 1, 2: 2, 5: 0, 6: 1, 4: 0, 7: 0}
+	unvisited := make(map[int]struct{}, len(k2d))
+	for key := range k2d {
+		unvisited[key] = struct{}{}
+	}
+
+	validateDegree := func(node *FibonacciHeapNode) {
+		if node == nil {
+			return
+		}
+		key := node.key
+		degree, ok := k2d[key]
+		if !ok {
+			t.Errorf("Unexpected key %d", key)
+		}
+		if degree != node.degree {
+			t.Errorf("For key %d got degree %d, want %d", key, node.degree, degree)
+		}
+		delete(unvisited, key)
+	}
+
+	walkFibonacciHeap(fheap, validateDegree)
+	if len(unvisited) > 0 {
+		t.Errorf("There are unvisited keys: %v", unvisited)
+	}
+
+	fheap = NewFibonacciHeap()
+
+	fhn := &fHeapNode{
+		key: 3,
+		children: []*fHeapNode{
+			{
+				key: 18,
+				children: []*fHeapNode{
+					{
+						key: 39,
+					},
+				},
+			},
+			{
+				key: 52,
+			},
+			{
+				key: 38,
+				children: []*fHeapNode{
+					{
+						key: 41,
+					},
+				},
+			},
+		},
+	}
+	node := fhn.FibonacciHeapNode()
+	fheap.Insert(node)
+
+	fhn = &fHeapNode{
+		key: 17,
+		children: []*fHeapNode{
+			{
+				key: 30,
+			},
+		},
+	}
+	node = fhn.FibonacciHeapNode()
+	fheap.Insert(node)
+
+	fhn = &fHeapNode{
+		key: 24,
+		children: []*fHeapNode{
+			{
+				key: 26,
+				children: []*fHeapNode{
+					{
+						key: 35,
+					},
+				},
+			},
+			{
+				key: 46,
+			},
+		},
+	}
+	node = fhn.FibonacciHeapNode()
+	fheap.Insert(node)
+
+	fheap.Insert(NewFibonacciHeapNode(23))
+	fheap.Insert(NewFibonacciHeapNode(7))
+	fheap.Insert(NewFibonacciHeapNode(21))
+
+	k2d = map[int]int{23: 0, 7: 0, 21: 0, 3: 3, 18: 1, 39: 0, 52: 0, 38: 1, 41: 0, 17: 1, 30: 0, 24: 2, 26: 1, 35: 0, 46: 0}
+	unvisited = make(map[int]struct{}, len(k2d))
+	for key := range k2d {
+		unvisited[key] = struct{}{}
+	}
+	walkFibonacciHeap(fheap, validateDegree)
+	if len(unvisited) > 0 {
+		t.Errorf("There are unvisited keys: %v", unvisited)
+	}
+
+	fheap.ExtractMin()
+	res = fmt.Sprint(fheap)
+	want = "(⟷ 7(⟷ 24(⟷ 26(⟷ 35() ⟷) ⟷ 46() ⟷) ⟷ 17(⟷ 30() ⟷) ⟷ 23() ⟷) ⟷ 18(⟷ 21(⟷ 52() ⟷) ⟷ 39() ⟷) ⟷ 38(⟷ 41() ⟷) ⟷)"
+	if res != want {
+		t.Errorf("fheap after ExtractMin = %s, want %s", res, want)
+	}
+	min = fheap.min.key
+	wantMin = 7
+	if min != wantMin {
+		t.Errorf("min = %d, want %d", min, wantMin)
+	}
+	k2d = map[int]int{7: 3, 24: 2, 26: 1, 35: 0, 46: 0, 17: 1, 30: 0, 23: 0, 18: 2, 21: 1, 52: 0, 39: 0, 38: 1, 41: 0}
+	unvisited = make(map[int]struct{}, len(k2d))
+	for key := range k2d {
+		unvisited[key] = struct{}{}
+	}
+	walkFibonacciHeap(fheap, validateDegree)
+	if len(unvisited) > 0 {
+		t.Errorf("There are unvisited keys: %v", unvisited)
 	}
 }
